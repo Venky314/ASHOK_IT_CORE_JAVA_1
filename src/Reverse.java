cb3f48ece8f6b9fd8ffd63f9f0dfdfdfd74b028a
venkatesh <lankevenkatesh314@gmail.com>
2025-08-23 15:12:56 +0530

    Reverse.java

---
+++ src/Reverse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter any name:- ");
+        Scanner scanner=new Scanner(System.in);
+        String name =scanner.nextLine();
+
+        int Lenthofname=name.length();
+
+        String emptyString ="";
+
+        for (int i=0;i<Lenthofname;i++){
+            emptyString+=name.charAt(Lenthofname-i-1);
+
+        }
+        System.out.println("this is the reverse string:--- "+emptyString);
+
+    }
+}
